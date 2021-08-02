3f0711d84f1d8acf1a1c42b5ad18cd4a558c0908
José Carlos López Arriaga <carlos_gara_1507@hotmail.com>
2021-08-02 14:40:04 -0500

    Upload code

---
+++ scbimg.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib as mpl
+
+def img2uint8(img):
+    vmin = img.min()
+    vmax = img.max()
+
+    img = ((img - vmin) / (vmax - vmin)) * 255.0
+
+    return np.uint8(img)
+
+def plothist(h, color):
+    fig, ax = plt.subplots(1, 1)
+
+    ax.stem(h, linefmt = '{}-'.format(color), markerfmt = 'none', basefmt = 'k-', use_line_collection=True)
+    ax.set_xlim(0, 255)
+    ax.grid('on')
+    ax.set_xticklabels([])
+
+    cmap = plt.get_cmap('gray', 255)
+    norm = mpl.colors.Normalize(vmin=0, vmax=255)
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+
+    fig.colorbar(sm, orientation='horizontal')
+    plt.show()
+
+def hist(img):
+    if len(img.shape) > 2:
+        raise ValueError('Image not in grayscale')
+
+    h = np.zeros((256, 1))
+
+    for m in range(img.shape[0]):
+        for n in range(img.shape[1]):
+            h[img[m, n]] += 1
+
+    return h
+
+def cumhist(img):
+    h = hist(img)
+
+    for i in range(1, len(h)):
+        h[i] += h[i - 1]
+
+    return h
+
+def searchvmin(h, nx, ny, s1):
+    vmin = h.tolist().index(min(min(h)))
+    n = nx * ny
+    while h[vmin + 1] <= n * (s1 / 100):
+        vmin += 1
+
+    return int(vmin)
+
+def searchvmax(h, nx, ny, s2):
+    vmax = h.tolist().index(max(max(h))) - 1
+    n = nx * ny
+    while h[vmax - 1] > n * (1 - (s2 / 100)):
+        vmax -= 1
+
+    if vmax < h.tolist().index(max(max(h))) - 1:
+        vmax += 1
+
+    return int(vmax)
+
+def saturate_rescale_pixels(img, vmin, vmax):
+    img = np.where(img < vmin, vmin, img)
+    img = np.where(img > vmax, vmax, img)
+
+    for m in range(img.shape[0]):
+        for n in range(img.shape[1]):
+            img[m, n] = (img[m, n] - vmin) * (255 / (vmax - vmin))
+
+    return img
+
+def scb(img, s1, s2):
+    if not (1 <= s1 <= 20 and 1 <= s2 <= 20):
+        raise ValueError('val min = 1, val max = 20')
+
+    if img.dtype != np.uint8:
+        img = img2uint8(img)
+
+    out = np.zeros(img.shape)
+
+    if len(img.shape) > 2:
+        # RGB image
+        for d in range(img.shape[2]):
+            vmin = searchvmin(cumhist(img[:, :, d]), img.shape[0], img.shape[1], s1)
+            vmax = searchvmax(cumhist(img[:, :, d]), img.shape[0], img.shape[1], s2)
+            out[:, :, d] = saturate_rescale_pixels(img[:, :, d], vmin, vmax)
+    else:
+        # Grayscale image
+        vmin = searchvmin(cumhist(img), img.shape[0], img.shape[1], s1)
+        vmax = searchvmax(cumhist(img), img.shape[0], img.shape[1], s2)
+        out = saturate_rescale_pixels(img, vmin, vmax)
+
+    return np.uint8(out)
+
+
+if __name__ == '__main__':
+    img = mpimg.imread('./images/lenna.png')
+
+    s1 = 1.5
+    s2 = 1.5
+
+    print('Wait...')
+    out = scb(img, s1, s2)
+
+    fig, (img_og, img_scb) = plt.subplots(ncols=2, nrows=1)
+
+    img_og.imshow(img, cmap='gray')
+    img_og.set_title('Input origunal image')
+    img_og.set_xticks([]), img_og.set_yticks([])
+
+    img_scb.imshow(out, cmap='gray')
+    img_scb.set_title('Output Simplest Color Balance image \n $S_1$ = {} \n $S_2$ = {}'.format(s1, s2))
+    img_scb.set_xticks([]), img_scb.set_yticks([])
+
+    plt.show()
